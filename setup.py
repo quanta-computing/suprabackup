6cde204d57417288c590bfd3311998263d6542a7
Matthieu Rosinski <korrigan@slash16.org>
2014-12-01 18:12:27 +0100

    added setuptools config

---
+++ setup.py
@@ -0,0 +1,43 @@
+"""
+Setup script for suprabackup
+
+"""
+
+from setuptools import setup
+
+
+def readme():
+    """
+    Extracts contents from README.md
+
+    """
+    with open('README.md') as r:
+        return r.read()
+
+def requirements():
+    """
+    Fetch requirements from requirements.txt
+
+    """
+    with open('requirements.txt') as req:
+        return req.read().splitlines()
+
+
+setup(name='suprabackup',
+      version='0.1',
+      description='Suprabackup is a tool to automate xtrabackup db backups'
+      long_description=readme(),
+      license='MIT',
+      url='https://github.com/quanta-computing/suprabackup'
+      author="Matthieu 'Korrigan' Rosinski",
+      author_email='mro@quanta-computing.com'
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Programming Language :: Python',
+          'License :: OSI Approved :: MIT License',
+          'Topic :: Database',
+          'Topic :: System :: Archiving :: Backup',
+      ],
+      packages=['suprabackup'],
+      install_requires=requirements(),
+  )
