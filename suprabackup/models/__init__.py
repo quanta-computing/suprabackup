19cf3976dee9c2ee78fdbda81a055f326b8020e4
Matthieu Rosinski <korrigan@slash16.org>
2018-05-14 18:40:27 +0200

    python3 compat

--- suprabackup/models/__init__.py
+++ suprabackup/models/__init__.py
@@ -3,6 +3,6 @@ This packages contains mappings for the ORM about suprabackup objects
 This module basically do nothing except providing models
 
 """
-from host import Host
-from schedule import Schedule
-from job import Job, JobStatus
+from .host import Host
+from .schedule import Schedule
+from .job import Job, JobStatus
